2b1a933b587fb5c2122cc6913366c1769e50e048
yoavthewk <yoavarviv1@gmail.com>
2021-12-28 20:29:48 +0200

    documented 23

--- Maman23/seek.c
+++ Maman23/seek.c
@@ -1,38 +1,44 @@
 #include "seek.h"
 
 FILE* openFile(char* fp);
+int getSize(FILE* fp);
 
 int main(int argc, char* argv[]){
 	int target, numOfFiles;
 	int index, sizeOfFile, charToRead;
+	
+	/* if argc < 2, we did not receive enough argument and we display an error. */
 	if(argc < 2){
 		printf("Too little arguments sent");
 		return 1;
 	}
+	
 	target = strtol(argv[1], NULL, 10);
 	
+	/* if target is 0 or negative, the target destination is invalid and we display an error. */
 	if(target <= 0){
-		printf("Number of file should be bigger than 0!");
+		printf("Target destination should be bigger than 0!");
 		return 1;
 	}
 	
 	numOfFiles = argc - 2;
 	index = 2;
 	while(numOfFiles > 0){
+		/* getting the file pointer */
 		FILE* fp = openFile(argv[index++]);
+		
 		if(fp){
-			fseek(fp, 0, SEEK_END);
-			sizeOfFile = ftell(fp);
+			sizeOfFile = getSize(fp);
+		
 			if(sizeOfFile < target){
 				printf("Number of characters in file is smaller than the argument passed.");
-			}S
-			else{
-				fseek(fp, target - 1, SEEK_SET);
-				charToRead = fgetc(fp);
-				printf("%c\n", charToRead);
 			}
-			
-			fclose(fp);
+			else{
+				fseek(fp, target - 1, SEEK_SET); /* getting the cursor to the spot before the char to read */
+				charToRead = fgetc(fp); /* reading the character */
+				printf("%c\n", charToRead); /* printing the character */
+			}
+			fclose(fp); /* if we successfully opened the file, we close it */
 		}
 		numOfFiles--;
 	}
@@ -42,10 +48,17 @@ int main(int argc, char* argv[]){
 
 FILE* openFile(char* fp){
 	FILE* temp = fopen(fp, "r");
+	
 	if(!temp){
 		printf("Cannot open file, invalid input");
 		return NULL;
 	}
+	
 	return temp;
+}
+
+int getSize(FILE* fp){
+	fseek(fp, 0, SEEK_END);
+	return ftell(fp);
 }
 
