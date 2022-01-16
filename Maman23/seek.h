19bdb5f1b16aa78e9586d4ad320009f951755286
Yoav Sakal Arviv <58586004+yoavthewk@users.noreply.github.com>
2022-01-16 13:38:01 +0200

    Update seek.h

--- Maman23/seek.h
+++ Maman23/seek.h
@@ -1,2 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+/*
+ * Opens the file and returns the file pointer
+*/
+FILE* openFile(char* fp);
+
+/* 
+ * This function gets a file pointer 
+*/
+int getSize(FILE* fp);
