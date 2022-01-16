060fa0f6921367c669b8679f295c481ba19f166c
Yoav Sakal Arviv <58586004+yoavthewk@users.noreply.github.com>
2022-01-16 13:37:48 +0200

    Update seek.c

--- Maman23/seek.c
+++ Maman23/seek.c
@@ -1,8 +1,5 @@
 #include "seek.h"
 
-FILE* openFile(char* fp);
-int getSize(FILE* fp);
-
 int main(int argc, char* argv[]){
 	int target, numOfFiles;
 	int index, sizeOfFile, charToRead;
@@ -13,23 +10,25 @@ int main(int argc, char* argv[]){
 		return 1;
 	}
 	
-	target = strtol(argv[1], NULL, 10);
+	target = strtol(argv[1], NULL, 10); /* get the target from the command line arguments. */
 	
 	/* if target is 0 or negative, the target destination is invalid and we display an error. */
 	if(target <= 0){
-		printf("Target destination should be bigger than 0!");
+		printf("Target destination should be bigger than 0!"); 
 		return 1;
 	}
 	
 	numOfFiles = argc - 2;
 	index = 2;
+	
 	while(numOfFiles > 0){
 		/* getting the file pointer */
 		FILE* fp = openFile(argv[index++]);
 		
 		if(fp){
-			sizeOfFile = getSize(fp);
-		
+			sizeOfFile = getSize(fp); /* get the size of the file */
+			
+			/* If the size of the file is smaller than the target, we display an error */
 			if(sizeOfFile < target){
 				printf("Number of characters in file is smaller than the argument passed.");
 			}
@@ -46,6 +45,9 @@ int main(int argc, char* argv[]){
 	return 0;
 }
 
+/*
+ * This function opens a file and returns the file pointer.
+*/
 FILE* openFile(char* fp){
 	FILE* temp = fopen(fp, "r");
 	
@@ -57,6 +59,9 @@ FILE* openFile(char* fp){
 	return temp;
 }
 
+/*
+ * This function returns the size of the file.
+*/
 int getSize(FILE* fp){
 	fseek(fp, 0, SEEK_END);
 	return ftell(fp);
